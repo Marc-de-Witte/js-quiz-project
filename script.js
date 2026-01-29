36f7c387734bb34274bcff03848d3704bc482cb9
Marc-de-Witte <99084882@mydavinci.nl>
2026-01-29 09:10:44 +0100

    voeg de vragen en de variabelen toe

---
+++ script.js
@@ -0,0 +1,56 @@
+const vragen = [
+  {
+    vraag: "Welke planeet staat het dichtst bij de zon?",
+    opties: ["Mercurius", "Venus", "Mars", "Aarde"],
+    correctIndex: 0
+  },
+  {
+    vraag: "Hoeveel graden is een rechte hoek?",
+    opties: ["120 graden", "90 graden", "180 graden", "45 graden"],
+    correctIndex: 1
+  },
+  {
+    vraag: "Welke oceaan is de grootste ter wereld?",
+    opties: ["Indische Oceaan", "Arctische Oceaan", "Stille Oceaan", "Atlantische Oceaan"],
+    correctIndex: 2
+  },
+  {
+    vraag: "Welke kleur krijg je als je blauw en geel mengt?",
+    opties: ["Oranje", "Groen", "Paars", "Bruin"],
+    correctIndex: 1
+  },
+  {
+    vraag: "Hoeveel poten heeft een spin?",
+    opties: ["12", "8", "6", "10"],
+    correctIndex: 1
+  },
+  {
+    vraag: "Welke stad is de hoofdstad van Frankrijk?",
+    opties: ["Parijs", "Nice", "Marseille", "Lyon"],
+    correctIndex: 0
+  },
+  {
+    vraag: "Wat is het grootste interne orgaan in het menselijk lichaam?",
+    opties: ["Nieren", "Lever", "Hart", "Longen"],
+    correctIndex: 1
+  },
+  {
+    vraag: "Welke vorm heeft precies drie zijden?",
+    opties: ["Rechthoek", "Vierkant", "Driehoek", "Vijfhoek"],
+    correctIndex: 2
+  },
+  {
+    vraag: "Welke dierensoort kan vliegen?",
+    opties: ["Pinguïn", "Kip", "Struisvogel", "Adelaar"],
+    correctIndex: 3
+  },
+  {
+    vraag: "Hoeveel minuten zitten er in een uur?",
+    opties: ["45", "30", "90", "60"],
+    correctIndex: 3
+  }
+];
+huidige_vraag = document.getElementById("vragen");
+score = document.getElementById("score");
+console.log(vragen)
+
